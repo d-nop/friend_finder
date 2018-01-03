f274b355c32ae9522204cad8a55fd442365d6384
Daniel Nop <danielnop@daniels-air.lan>
2018-01-02 22:26:36 -0500

    background images added, new user data is added to array

---
+++ app/data/friends.js
@@ -0,0 +1,198 @@
+//Creating an array of friends and their choices
+
+var friendArray = [
+{
+name: "Darth Vadar",
+photo: "https://www.independent.ie/regionals/kerryman/news/article34250758.ece/BINARY/2015-12-02_ker_15039481_I1.JPG",
+scores: [
+"5",
+"1",
+"4",
+"4",
+"5",
+"1",
+"2",
+"5",
+"4",
+"1"
+]
+},
+{
+name: "Jacob Deming",
+photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+scores: [
+"4",
+"2",
+"5",
+"1",
+"3",
+"2",
+"2",
+"1",
+"3",
+"2"
+]
+},
+{
+name: "Jeremiah Scanlon",
+photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+scores: [
+"5",
+"2",
+"2",
+"2",
+"4",
+"1",
+"3",
+"2",
+"5",
+"5"
+]
+},
+{
+name: "Louis T. Delia",
+photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+scores: [
+"3",
+"3",
+"4",
+"2",
+"2",
+"1",
+"3",
+"2",
+"2",
+"3"
+]
+},
+{
+name: "Lou Ritter",
+photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+scores: [
+"4",
+"3",
+"4",
+"1",
+"5",
+"2",
+"5",
+"3",
+"1",
+"4"
+]
+},
+{
+name: "Jordan Biason",
+photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+scores: [
+"4",
+"4",
+"2",
+"3",
+"2",
+"2",
+"3",
+"2",
+"4",
+"5"
+]
+},
+{
+name: "newby mcgee",
+photo: "http://dreamstop.com/wp-content/uploads/2016/08/llama-dreams.jpg",
+scores: [
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3"
+]
+},
+{
+name: "Jar Jar",
+photo: "http://www.ranthollywood.com/wp-content/uploads/2015/03/Jjportrait.jpg",
+scores: [
+"1",
+"1",
+"3",
+"1",
+"2",
+"3",
+"5",
+"1",
+"1",
+"5"
+]
+},
+{
+name: "BB-8",
+photo: "https://heavyeditorial.files.wordpress.com/2016/09/bb8-costume.jpg?quality=65&strip=all&strip=all",
+scores: [
+"4",
+"3",
+"2",
+"1",
+"4",
+"2",
+"4",
+"1",
+"3",
+"5"
+]
+},
+{
+name: "Joe",
+photo: "https://www.pexels.com/photo/woman-on-rock-platform-viewing-city-196667/",
+scores: [
+"5",
+"1",
+"3",
+"1",
+"5",
+"3",
+"5",
+"1",
+"1",
+"5"
+]
+},
+{
+name: "Greg",
+photo: "http://fabriccreative.com/portfolio/photo-grace-face",
+scores: [
+"1",
+"2",
+"3",
+"1",
+"5",
+"5",
+"5",
+"3",
+"1",
+"2"
+]
+},
+{
+name: "John Smith",
+photo: "https://www.biography.com/.image/t_share/MTIwNjA4NjMzOTc0MTk1NzI0/john-smith-9486928-1-402.jpg",
+scores: [
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3",
+"3"
+]
+}
+]
+// exporting array.  This makes it accessible to other files using require.
+module.exports = friendArray;
